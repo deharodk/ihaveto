5a3b374592d3a941b3c10f83f28ef3f514468992
Luis de Haro <luis.deharo@diverza.com>
2014-11-22 20:49:55 -0600

    Recreated sqlite database

--- db/migrate/20141123013413_create_users.rb
+++ db/migrate/20141123013413_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email
       t.string :name
       t.boolean :is_active
 
       t.timestamps
     end
   end
+
 end
