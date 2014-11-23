92f7b8723b6838060af62d0e71ebeb2178e8cb18
Luis de Haro <luis.deharo@diverza.com>
2014-11-22 20:32:32 -0600

    Devise added and configurated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ihaveto::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +54,6 @@ Ihaveto::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get '/', to: 'home#index', as: 'root_url'
 end
